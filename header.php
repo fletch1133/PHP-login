1c7a29412c443b2c98d90cb31bcf6834579667e2
Anthony Fletcher <afletcher2433.com>
2023-11-17 13:04:05 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Project 1</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">About</a></li>
+                <li><a href="blog.php">Find Blogs</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+        </div>
+    </nav>
+
+<div class="wrapper">
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+    include_once 'header.php';
+?>
+
+    <section class="index-intro">
+        <h1>This is an Introduction</h1>
+        <p>Here is a paragraph that explains the purpose of the website</p>
+    </section>
+
+    <section class="index-categories">
+        <h2>Some basic categories</h2> 
+        <div class="index-categories-list">
+            <div>
+                <h3>Fun Things</h3>
+            </div>
+            <div>
+                <h3>Serious Items</h3>
+            </div>
+            <div>
+                <h3>Exciting Items</h3>
+            </div>
+            <div>
+                <h3>Boring Things</h3> 
+            </div>
+        </div> 
+    </section>
+    
+    <?php
+    include_once 'footer.php'; 
+?>
